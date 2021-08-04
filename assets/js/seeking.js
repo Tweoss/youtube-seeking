9f042c86cc749196cddf518b899e35c53107e2de
Francis Chua <francis@opennetworking.org>
2021-08-03 22:44:05 -0700

    footer, jump

--- assets/js/seeking.js
+++ assets/js/seeking.js
@@ -21,6 +21,8 @@ var player,
     repeat_count = 0,
     // length of buffer size in seconds
     buffer_size = 0,
+    // whether or not to jump to the next segment
+    jump_next = false,
     // current segment's start and end accounting for buffer
     current_segment = { start: -Infinity, end: -Infinity },
     // current index, used to check if segment needs to be updated
@@ -102,6 +104,9 @@ function onPlayerReady(pEvent) {
             repeat_count = e.target.value;
             document.getElementById("repeatCountText").textContent = e.target.value;
         });
+        document.getElementById("jumpNext").addEventListener("input", function(e) {
+            jump_next = e.target.value;
+        });
     })
 }
 
@@ -118,7 +123,7 @@ function rewriteStamps() {
             })
             player.seekTo(times[0] - buffer_size, true);
             // give priority to this segment
-            current_segment = { start: times[0] - buffer_size, end: times[1] - buffer_size };
+            current_segment = { start: times[0] - buffer_size, end: times[1] + buffer_size };
             current_index = index;
             current_repeat = 0;
             segment_jumped = true;
@@ -132,18 +137,33 @@ function rewriteStamps() {
 // sets a timeout for when to call again, sets current_segment, current_repeat, current_index
 function searchAndHighlight() {
     const seconds = player.getCurrentTime();
+    // increase time progress bar
     if (!segment_jumped && seconds >= current_segment.start && seconds <= current_segment.end) {
         const bar = document.querySelector("#timeProgress > div");
         bar.style.width = `${((seconds - current_segment.start) / (current_segment.end - current_segment.start) * 100)}%`;
         bar.textContent = `${((seconds - current_segment.start) / (current_segment.end - current_segment.start) * 100).toFixed(0)}%`;
         return;
-    } else if (seconds > current_segment.end && seconds < current_segment.end + 2 && current_repeat < repeat_count) {
+    }
+    // jump to the start for repeat
+    else if (seconds > current_segment.end && seconds < current_segment.end + 2 && current_repeat < repeat_count) {
         current_repeat++;
         player.seekTo(current_segment.start, true);
         const bar = document.querySelector("#repeatProgress > div");
         bar.style.width = `${current_repeat * 100 / repeat_count}%`;
         bar.textContent = `${current_repeat}/${repeat_count}`;
-    } else {
+    }
+    // jump to the next segment
+    else if (seconds > current_segment.end && seconds < current_segment.end + 2 && jump_next) {
+        player.seekTo(timeToSeconds(time_object[current_index + 1].start) - buffer_size, true);
+        current_segment = { start: timeToSeconds(time_object[current_index + 1].start) - buffer_size, end: timeToSeconds(time_object[current_index + 1].end) + buffer_size };
+        // give priority to this segment
+        current_index = current_index + 1;
+        current_repeat = 0;
+        segment_jumped = true;
+        resetBars();
+    }
+    // main logic to find and set segments, color, and set progress bars
+    else {
 
         let [index, closest_start] = findContains(time_object, seconds, buffer_size);
 
